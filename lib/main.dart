5b9795022e7367a5908bcc57aea05c6517329781
unknown <usuario1@M2-305-07-11366.utadeo.loc>
2023-03-02 21:18:21 -0500

    Primera pantalla completa

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,11 @@ import 'package:flutter/material.dart';
     }
 
     class MainView extends StatefulWidget{
-
         @override
         State <MainView> createState() => _MainView();
     }
 
     class _MainView extends State<MainView>{
-
     @override
     Widget build(BuildContext context){
         return MaterialApp(home: InfoPage());
@@ -19,21 +17,59 @@ import 'package:flutter/material.dart';
 }
 
 class InfoPage extends StatelessWidget{
-
     @override
     Widget build(BuildContext context){
         return Scaffold(
-            appBar: AppBar(),
-            body: Center (child: Column(
+            body: Container(
+              margin: 
+                const EdgeInsets.only(left: 30, right: 30, top: 90, bottom: 50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                     Image.asset('assets/image2.png'),
-                    Text('lorem loremloremloremlorem lorem'),
-                    Text('Pruebas pruebas ´pruebas,d dsfljksndfkjdsf ds'),
-                    TextButton(child: Text('Masuk')),
-                    TextButton(child: Text('Masuk'))
-                ],
-            )
-            ))
-            );
-    }
+                    const Text('Peque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur',
+                    textAlign: TextAlign.center,
+                    style: 
+                    TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+                const Text(
+                    'Beli kendaraan sesuai minatmu dan dengan harga negosiasi terbaik',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16)),
+                ElevatedButton(
+                    onPressed: () => {},
+                    style: OutlinedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: Text('Masuk')),
+                OutlinedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => RegisterPage()));
+                    },
+                    style: OutlinedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                      side: const BorderSide(width: 2.0, color: Colors.blue),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    child: const Text('Mendaftar'))
+              ],
+            )));
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+          child: const Text('back'))
+    ]));
+  }
 }
